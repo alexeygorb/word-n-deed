a8a12e7f2a801c71e230a9a30185c8c9b2196b4d
Alexey Gorb <xalgo@berlingskemedia.dk>
2014-12-10 20:36:56 +0200

    Removed secondary menu and menu titles.

--- sites/all/themes/subclean/template.php
+++ sites/all/themes/subclean/template.php
@@ -1,4 +1,11 @@
 <?php
 
+/**
+ * Implements hook_preprocess_page().
+ */
+function subclean_preprocess_page(&$variables) {
 
+  unset($variables['page']['main_menu']['#heading']);
 
+  unset($variables['page']['secondary_menu']);
+}
