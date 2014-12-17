8c8ea2bf5190ddcc476703630b5faa2bc0d915e2
Alexey Gorb <gorb.alexey@gmail.com>
2014-12-17 22:33:03 +0200

    Finilized the form...

--- sites/all/modules/word/word-add-word.tpl.php
+++ sites/all/modules/word/word-add-word.tpl.php
@@ -1,6 +1,6 @@
 <div class="node node-card node-teaser node-type-card node-teaser-card style-one" id="node-card-add">
   <div class="node-content clearfix">
-    <div class="field field-name-field-card-photo field-type-image"><div class="field-items"><div class="field-item even"><img typeof="foaf:Image" src="<?php print $directory . '/images/profile.jpg'; ?>" width="125" height="125" alt="Huic ideo ludus paratus proprius quadrum quis vero." title="Diam haero nibh quae."></div></div></div>
+    <div class="field field-name-field-card-photo field-type-image"><div class="field-items"><div class="field-item even"><img typeof="foaf:Image" src="<?php print $directory . '/images/profile.jpg'; ?>" width="110" height="110"></div></div></div>
     <div class="field field-name-field-text field-type-text-long"><div class="field-items"><div class="field-item even"><a href="#">Зігріти словом</a></div></div></div>
   </div>
 </div>
