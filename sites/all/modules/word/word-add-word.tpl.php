a48141962cc079b589b72730a9b0e6c6a6478cea
bmxalvo <asvsot@gmail.com>
2014-12-18 05:17:16 +0200

    styling

--- sites/all/modules/word/word-add-word.tpl.php
+++ sites/all/modules/word/word-add-word.tpl.php
@@ -1,6 +1,18 @@
 <div class="node node-card node-teaser node-type-card node-teaser-card style-one" id="node-card-add">
   <div class="node-content clearfix">
-    <div class="field field-name-field-card-photo field-type-image"><div class="field-items"><div class="field-item even"><img typeof="foaf:Image" src="<?php print $directory . '/images/profile.jpg'; ?>" width="110" height="110"></div></div></div>
-    <div class="field field-name-field-text field-type-text-long"><div class="field-items"><div class="field-item even"><a href="#">Зігріти словом</a></div></div></div>
+    <div class="field field-name-field-card-photo field-type-image">
+      <div class="field-items">
+        <div class="field-item even">
+          <img typeof="foaf:Image" src="<?php print $directory . '/images/profile.jpg'; ?>" width="110" height="110">
+          <div class="diamond"></div>
+        </div>
+      </div>
+    </div>
+    <div class="field field-name-field-text field-type-text-long">
+      <div class="field-items"><div class="field-item even">
+          <a href="#">Зігріти словом</a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
